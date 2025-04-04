5c27efd7258c3d0e9856dd591952762fd0fe5bab
Fabrizio Mettini <fabrizioamettini@gmail.com>
2025-04-04 20:55:05 -0300

    Agrega semáforo a `thread_test_simple.cc`
    Agrega semáforo inicializado a `thread_test_simple.cc`. Además se proveen mensajes de depuración para las operaciones del mismo.

--- code/threads/thread_test_simple.cc
+++ code/threads/thread_test_simple.cc
@@ -6,11 +6,11 @@
 
 
 #include "thread_test_simple.hh"
+#include "semaphore.hh"
 #include "system.hh"
 
 #include <stdio.h>
 #include <string.h>
-
 
 /// Loop 10 times, yielding the CPU to another ready thread each iteration.
 ///
@@ -21,6 +21,11 @@
 static const unsigned N_THREADS = 4; 
 static const char *names[N_THREADS] = {"2nd", "3rd", "4th", "5th"};
 static bool done[N_THREADS] = {false};
+
+#ifdef SEMAPHORE_TEST
+static Semaphore sem = Semaphore(NULL, 3);
+#endif
+
 static void
 SimpleThread(void *name_)
 {
@@ -29,7 +34,19 @@ SimpleThread(void *name_)
     // behave incorrectly, because printf execution may cause race
     // conditions.
     for (unsigned num = 0; num < 10; num++) {
+
+#ifdef SEMAPHORE_TEST
+        sem.P();
+        DEBUG('s', "Thread %s has made a P() operation.\n", currentThread->GetName());        
+#endif
+
         printf("*** Thread `%s` is running: iteration %u\n", currentThread->GetName(), num);
+
+#ifdef SEMAPHORE_TEST
+        sem.V();
+        DEBUG('s', "Thread %s has made a V() operation.\n", currentThread->GetName());        
+#endif
+
         currentThread->Yield();
     }
     for (unsigned i = 0; i < N_THREADS; i++) {
@@ -38,7 +55,6 @@ SimpleThread(void *name_)
         }
     }
     printf("!!! Thread `%s` has finished SimpleThread\n", currentThread->GetName());
- 
 }
 
 /// Set up a ping-pong between several threads.
