4cf60f15d2ad0f90f448349b84837cd4fdcf322a
TizKah <tizianomanuel@gmail.com>
2025-04-06 17:14:47 -0300

    Agrego implementacion de channel

---
+++ code/threads/channel.cc
@@ -0,0 +1,62 @@
+#include "channel.hh"
+#include "condition.hh"
+#include "lock.hh"
+#include <stdio.h>
+#include "system.hh"
+
+
+Channel::Channel() {
+    condition_lock = new Lock("channel_lock");
+    sender_condition = new Condition("sender_condition", condition_lock);
+    receiver_condition = new Condition("receiver_condition", condition_lock);
+    bufferBusy = false;
+}
+
+Channel::~Channel() {
+    delete condition_lock;
+    delete sender_condition;
+    delete receiver_condition;
+}
+
+
+void Channel::Send(int message) {
+    condition_lock->Acquire();
+    
+    while(bufferBusy) {
+        printf("Thread %s esperando receptor...\n", currentThread->GetName());
+        sender_condition->Wait();
+    }
+    
+    // Seteo el mensaje
+    buffer = message;
+    bufferBusy = true;
+    printf("Thread %s mandó el mensaje: %d\n", currentThread->GetName(), buffer);
+    
+    // Le aviso al receiver que ya puede recibir el mensaje
+    receiver_condition->Signal();
+
+    condition_lock->Release();
+
+}
+
+
+void Channel::Receive(int *message) {
+    condition_lock->Acquire();
+    
+    while(!bufferBusy) {
+        printf("Thread %s esperando emisor...\n", currentThread->GetName());
+        receiver_condition->Wait();
+    }
+    
+    // Recibo el mensaje
+    *message = buffer;
+    bufferBusy = false;
+    printf("Thread %s recibió el mensaje: %d\n", currentThread->GetName(), *message);
+    
+    // Le aviso al sender que ya leí el mensaje
+    // Es decir, ya puede usar el buffer nuevamente
+    sender_condition->Signal();
+
+    condition_lock->Release();
+
+}
---
+++ code/threads/channel.hh
@@ -0,0 +1,27 @@
+
+
+
+#ifndef NACHOS_THREADS_CHANNEL__HH
+#define NACHOS_THREADS_CHANNEL__HH
+#include "lock.hh"
+#include "condition.hh"
+
+
+class Channel {
+public:
+    Channel();
+    ~Channel();
+
+    void Send(int message);
+    void Receive(int *message);
+
+private:
+    bool bufferBusy;
+    int buffer;
+    Condition* sender_condition;
+    Condition* receiver_condition;
+    Lock *condition_lock;
+};
+
+
+#endif
