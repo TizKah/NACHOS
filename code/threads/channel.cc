9ce59ee904065dc12004fda976c70ca77742e132
TizKah <tizianomanuel@gmail.com>
2025-05-25 21:00:07 -0300

    saco prints

--- code/threads/channel.cc
+++ code/threads/channel.cc
@@ -23,14 +23,14 @@ void Channel::Send(int message) {
     condition_lock->Acquire();
     
     while(buffer_busy) {
-        printf("Thread %s esperando receptor...\n", currentThread->GetName());
+        // printf("Thread %s esperando receptor...\n", currentThread->GetName());
         sender_condition->Wait();
     }
     
     // Seteo el mensaje
     buffer = message;
     buffer_busy = true;
-    printf("Thread %s mandó el mensaje: %d\n", currentThread->GetName(), buffer);
+    // printf("Thread %s mandó el mensaje: %d\n", currentThread->GetName(), buffer);
     
     // Le aviso al receiver que ya puede recibir el mensaje
     receiver_condition->Signal();
@@ -44,14 +44,14 @@ void Channel::Receive(int *message) {
     condition_lock->Acquire();
     
     while(!buffer_busy) {
-        printf("Thread %s esperando emisor...\n", currentThread->GetName());
+        // printf("Thread %s esperando emisor...\n", currentThread->GetName());
         receiver_condition->Wait();
     }
     
     // Recibo el mensaje
     *message = buffer;
     buffer_busy = false;
-    printf("Thread %s recibió el mensaje: %d\n", currentThread->GetName(), *message);
+    // printf("Thread %s recibió el mensaje: %d\n", currentThread->GetName(), *message);
     
     // Le aviso al sender que ya leí el mensaje
     // Es decir, ya puede usar el buffer nuevamente
